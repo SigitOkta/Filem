179620eadfbf781f20bd8a9cdb255512bcd4dbaf
oktasigit <oktasigit@gmail.com>
2023-08-08 21:54:29 +0700

    add bottom navbar

--- app/src/main/java/com/so/filem/ui/MainActivity.kt
+++ app/src/main/java/com/so/filem/ui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.so.filem.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.so.filem.R
 import com.so.filem.databinding.ActivityMainBinding
 import com.so.filem.ui.base.BaseActivity
@@ -13,5 +14,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        val navController = findNavController(R.id.nav_host_fragment)
+        binding.bottomNav.setupWithNavController(navController)
+
     }
 }
