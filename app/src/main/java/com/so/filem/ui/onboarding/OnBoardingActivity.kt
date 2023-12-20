0feccd68cc332610b28ad49aa0ec7145136e4d9a
oktasigit <oktasigit@gmail.com>
2023-12-20 11:24:10 +0700

    add splash layout + AppIntro library

---
+++ app/src/main/java/com/so/filem/ui/onboarding/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.so.filem.ui.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.so.filem.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
