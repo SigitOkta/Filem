6510ae1151fb2d4e857e352d7c2c44bd154c4107
oktasigit <oktasigit@gmail.com>
2023-12-20 10:54:29 +0700

    add splash layout + logo

---
+++ app/src/main/java/com/so/filem/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.so.filem.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.so.filem.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
