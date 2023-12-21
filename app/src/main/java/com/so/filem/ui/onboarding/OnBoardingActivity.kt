c7f27188f02d7f0fbb9fb4cf7f0cf84991cdaeb0
oktasigit <oktasigit@gmail.com>
2023-12-21 12:15:17 +0700

    SplashScreenActivity + OnBoardingActivity

--- app/src/main/java/com/so/filem/ui/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/so/filem/ui/onboarding/OnBoardingActivity.kt
@@ -1,12 +1,62 @@
 package com.so.filem.ui.onboarding
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.github.appintro.AppIntro2
+import com.github.appintro.AppIntroFragment
 import com.so.filem.R
+import com.so.filem.data.local.storepref.UserPreference
+import com.so.filem.ui.MainActivity
 
-class OnBoardingActivity : AppCompatActivity() {
+class OnBoardingActivity : AppIntro2(){
+    companion object{
+        private const val TAG = "OnBoardingActivity"
+    }
+
+    private val preference: UserPreference by lazy{
+        UserPreference(this@OnBoardingActivity)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        supportActionBar?.hide()
+        setupSliderFragment()
+    }
+
+    private fun setupSliderFragment() {
+        isSkipButtonEnabled = false
+        setIndicatorColor(
+            selectedIndicatorColor = ContextCompat.getColor(this, R.color.off_white_400),
+            unselectedIndicatorColor = ContextCompat.getColor(this, R.color.light_blue),
+        )
+        addSlide(
+            AppIntroFragment.createInstance(
+                description = getString(R.string.text_onboard_desc_1),
+                imageDrawable = R.drawable.ic_onboard_image_1,
+                descriptionTypefaceFontRes = R.font.inter_medium,
+                backgroundColorRes = R.color.dark_blue_600,
+                descriptionColorRes = R.color.off_white_400
+            )
+        )
+
+        addSlide(
+            AppIntroFragment.createInstance(
+                description = getString(R.string.text_onboard_desc_2),
+                imageDrawable = R.drawable.ic_onboard_image_2,
+                descriptionTypefaceFontRes = R.font.inter_medium,
+                backgroundColorRes = R.color.dark_blue_600,
+                descriptionColorRes = R.color.off_white_400
+            )
+        )
+    }
+
+    override fun onDonePressed(currentFragment: Fragment?) {
+        super.onDonePressed(currentFragment)
+        preference.setSkipIntro(true)
+        Intent(this@OnBoardingActivity, MainActivity::class.java).also {
+            startActivity(it)
+        }
     }
 }
--- app/src/main/java/com/so/filem/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/so/filem/ui/splash/SplashScreenActivity.kt
@@ -1,12 +1,49 @@
 package com.so.filem.ui.splash
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.so.filem.R
+import android.os.Handler
+import android.os.Looper
+import com.so.filem.data.local.storepref.UserPreference
+import com.so.filem.databinding.ActivitySplashScreenBinding
+import com.so.filem.ui.MainActivity
+import com.so.filem.ui.base.BaseActivity
+import com.so.filem.ui.onboarding.OnBoardingActivity
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity :
+    BaseActivity<ActivitySplashScreenBinding>(ActivitySplashScreenBinding::inflate) {
+
+
+    private val preference: UserPreference by lazy {
+        UserPreference(this@SplashScreenActivity)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        initView()
+        observeData()
     }
+
+
+    private fun initView() {
+        supportActionBar?.hide()
+    }
+
+    private fun observeData() {
+        if (preference.isSkipIntro()) {
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                })
+            }, 3000)
+
+        } else {
+            Handler(Looper.getMainLooper()).postDelayed({
+                startActivity(Intent(this@SplashScreenActivity, OnBoardingActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                })
+            }, 3000)
+        }
+    }
+
 }
