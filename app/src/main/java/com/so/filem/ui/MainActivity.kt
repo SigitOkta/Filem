4a971ba3a9e919fbae243de7057928a6acacf208
Sigit Okta <Oktasigit@gmail.com>
2024-03-02 23:58:54 +0700

    add fab for fav and forum

--- app/src/main/java/com/so/filem/ui/MainActivity.kt
+++ app/src/main/java/com/so/filem/ui/MainActivity.kt
@@ -6,22 +6,65 @@
 
 package com.so.filem.ui
 
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.so.filem.R
-import com.so.filem.databinding.ActivityMainBinding
 import com.so.filem.base.BaseActivity
+import com.so.filem.databinding.ActivityMainBinding
+import com.so.filem.ui.custom.FabManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-
+    private var isExpanded = false
+    private val fabManager: FabManager by lazy { FabManager(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         val navController = findNavController(R.id.nav_host_fragment)
         binding.bottomNav.setupWithNavController(navController)
+        binding.mainFabBtn.setOnClickListener {
 
+            if (isExpanded) {
+                fabManager.shrinkFab(binding)
+                isExpanded = !isExpanded
+            } else {
+                fabManager.expandFab(binding)
+                isExpanded = !isExpanded
+            }
+
+        }
+
+    }
+
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+
+        if (ev?.action == MotionEvent.ACTION_DOWN) {
+            if (isExpanded) {
+                val outRect = Rect()
+                binding.fabConstraint.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(ev.rawX.toInt(), ev.rawY.toInt())) {
+                    fabManager.shrinkFab(binding)
+                    isExpanded = !isExpanded
+                    return dispatchTouchEvent(ev)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun onBackPressed() {
+
+        if (isExpanded) {
+            fabManager.shrinkFab(binding)
+            isExpanded = !isExpanded
+        } else {
+            super.onBackPressed()
+
+        }
     }
 }
---
+++ app/src/main/java/com/so/filem/ui/custom/FabManager.kt
@@ -0,0 +1,65 @@
+/*
+ * Created by SigitO
+ * Copyright (c) 2024 . All rights reserved.
+ * Last modified 3/2/24, 8:40 PM
+ */
+
+package com.so.filem.ui.custom
+
+import android.content.Context
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.so.filem.R
+import com.so.filem.databinding.ActivityMainBinding
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+class FabManager(context : Context) {
+    private val fromBottomFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(context, R.anim.from_bottom_fab)
+    }
+    private val toBottomFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(context, R.anim.to_bottom_fab)
+    }
+    private val rotateClockWiseFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(context, R.anim.rotate_clock_wise)
+    }
+    private val rotateAntiClockWiseFabAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(context, R.anim.rotate_anti_clock_wise)
+    }
+    private val fromBottomBgAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(context, R.anim.from_bottom_anim)
+    }
+    private val toBottomBgAnim: Animation by lazy {
+        AnimationUtils.loadAnimation(context, R.anim.to_bottom_anim)
+    }
+
+    private fun onGalleryClicked(context: Context) {
+        Toast.makeText(context, "Gallery Clicked", Toast.LENGTH_SHORT).show()
+    }
+
+    fun shrinkFab(binding: ActivityMainBinding) {
+
+        binding.transparentBg.startAnimation(toBottomBgAnim)
+        binding.mainFabBtn.startAnimation(rotateAntiClockWiseFabAnim)
+        binding.favFabBtn.startAnimation(toBottomFabAnim)
+        binding.forumFabBtn.startAnimation(toBottomFabAnim)
+        binding.tvFabFav.startAnimation(toBottomFabAnim)
+        binding.tvFabForum.startAnimation(toBottomFabAnim)
+    }
+
+    fun expandFab(binding: ActivityMainBinding) {
+
+        binding.transparentBg.startAnimation(fromBottomBgAnim)
+        binding.mainFabBtn.startAnimation(rotateClockWiseFabAnim)
+        binding.favFabBtn.startAnimation(fromBottomFabAnim)
+        binding.forumFabBtn.startAnimation(fromBottomFabAnim)
+        binding.tvFabFav.startAnimation(fromBottomFabAnim)
+        binding.tvFabForum.startAnimation(fromBottomFabAnim)
+    }
+}
+
